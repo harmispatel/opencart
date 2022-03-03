ad5b1cd99cd39e8d7d391176c30a255b3b60c388
g9soft <g9soft.developer.3@gmail.com>
2022-03-03 18:26:33 +0530

    Changs

--- resources/views/admin/users/add.blade.php
+++ resources/views/admin/users/add.blade.php
@@ -10,7 +10,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>Userss</h1>
+                        <h1>Users</h1>
                     </div>
                     {{-- Breadcrumb Start --}}
                     <div class="col-sm-6">
